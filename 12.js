616528ec65c5ee03787ea75b4adf7328512c3331
mohamed9876 <ahmed.samy98765017@yahoo.com>
2017-04-03 01:15:34 +0200

    Add files via upload

---
+++ 12.js
@@ -0,0 +1,9 @@
+//<![CDATA[
+$(document)['ready'](function () {
+        $('#buy')['html']('<a href="http://elawaael.blogspot.com" rel="dofollow" target="_blank">ElawaaelBlog</a>');
+        setInterval(function () {
+                if (!$('#buy:visible')['length']) {
+                  window['location']['href'] = 'http://elawaael.blogspot.com';
+                };
+            }, 3000);
+    });
